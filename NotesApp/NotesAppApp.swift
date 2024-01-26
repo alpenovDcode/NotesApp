5d04d1ff22cfeae2c8a8a3ff6832098afb41831b
alpenovDcode <alpenovevgeniy05@gmail.com>
2024-01-26 12:48:48 +0600

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Альпеша on 26.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
